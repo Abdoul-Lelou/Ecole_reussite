c57dbbb028f2a72ee37e52f2abb3df7b246d6792
koumba6 <diongue901@gmail.com>
2022-10-03 16:22:46 +0000

    ajout eleve

---
+++ pages/ajoutEleve.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h2>ajoutEleve</h2>
+        <form action="">
+            <label for="nom">nom</label>
+            <input type="text" name="nom" placeholder="nom">
+            <label for="prenom">prenom</label>
+            <input type="text" name="prenom" placeholder="prenom">
+            <label for="username">username</label>
+            <input type="text" name="username"placeholder="username">
+            <label for="password">password</label>
+            <input type="text" name="password" placeholder="password">
+            <label for="age">age</label>
+            <input type="text" name="age" placeholder="age">
+            <label for="sexe">sexe</label>
+            <input type="text" name="sexe" placeholder="sexe">
+            <label for="nom">nom</label>
+            <input type="text" name="niveau" placeholder="niveau">
+            <label for="lieu de naissance">lieu de nassance</label>
+            <input type="text" name="lieu de naissance" placeholder="lieu de naissance">
+        </form>
+        <button type="submit">ajouter</button>
+    </div>
+</body>
+</html>
