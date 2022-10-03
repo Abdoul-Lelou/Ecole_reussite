9a982d9d8e5e7ead85e0c47a4c2f6d7cde8b1cdc
marie-cherif <maremesow422@gmail.com>
2022-10-03 09:32:33 +0000

    ok

--- model/model.php
+++ model/model.php
@@ -8,18 +8,13 @@
         { 
           try
             {
-<<<<<<< HEAD
                 $this->db= new PDO('mysql:host=localhost;dbname=Ecole_reussite;','root','');
-=======
-                $this->db= new PDO('mysql:host=localhost;dbname=ecole_reussite;','root','');
->>>>>>> c96807c21ddd98ad754012dda657289f7729ef5a
             }catch(Exception $e)
             {
                 die("Connection erreur du à ".$e->getMessage());
             }
         }  
 
-<<<<<<< HEAD
         
         public function connect($username,$passwords){
             $sql= $this->db->prepare('SELECT user WHERE  = ');
@@ -56,16 +51,6 @@
             
             $sql=$this->db->prepare('INSERT INTO `user` ( `nom`, `prenom`, `age`, `sexe`,`username`,`passwords`,`roles`,`tel`,`matricule`,`lieu_naissance`,`niveau`,`email`)
                                             VALUES (:nom,:prenom,:age,:sexe,:username,:passwords,:roles,:tel,:matricule,:lieu_naissance,:niveau,:email)');
-=======
-        public function generateMatricule(){
-            
-        }
-       
-        public function ajoutEleve($nom,$prenom,$age,$sexe,$username,$passwords,$roles,$niveau,$lieu_naissance){
-            
-            try {
-                $sql=$this->db->prepare('INSERT INTO `user` ( `nom`, `prenom`, `age`, `sexe`,`username`,`passwords`,`roles`,`niveau`,`lieu_naissance`)
-                                            VALUES (:nom,:prenom,:age,:sexe,:username,:passwords,:roles,:niveau,:lieu_naissance)');
             
                         $sql->execute(array(
                         
@@ -76,58 +61,13 @@
                         'username' => $username,
                         'passwords' => $passwords,
                         'roles' => $roles,
-                        'niveau' => $niveau,
-                        'lieu_naissance' => $lieu_naissance
-                       
-                        ));
-                    // return $sql;
-                    if ($sql) {
-                        # code...
-                        echo "<script>alert('Inscription reussie')</script>";
-                        $sql->closeCursor();
-                    }
-            } catch (\Throwable $th) {
-                echo $th->getMessage();
-                $sql->closeCursor();
-            }
-        }
-
-        public function addUser($nom,$prenom,$age,$sexe,$username,$passwords,$roles,$matricule,$lieu_naissance=null,$email=null,$tel=null){
-            
-            try {
-                $sql=$this->db->prepare('INSERT INTO `user` ( `nom`, `prenom`, `age`, `sexe`,`username`,`passwords`,`roles`,`matricule`,`lieu_naissance`,`email`,`tel`)
-                                            VALUES (:nom,:prenom,:age,:sexe,:username,:passwords,:roles,:matricule,:lieu_naissance,:email,:tel)');
->>>>>>> c96807c21ddd98ad754012dda657289f7729ef5a
-            
-                        $sql->execute(array(
-                        
-                        'nom' =>$nom,
-                        'prenom' => $prenom,
-                        'age' => $age,
-                        'sexe' => $sexe,
-                        'username' => $username,
-                        'passwords' => $passwords,
-                        'roles' => $roles,
+                        'tel' => $tel,
                         'matricule' => $matricule,
                         'lieu_naissance' => $lieu_naissance,
-<<<<<<< HEAD
                         'niveau' => $niveau,
                         'email' => $email
-=======
-                        'email' => $email,
-                        'tel' => $tel
->>>>>>> c96807c21ddd98ad754012dda657289f7729ef5a
                         ));
-                    // return $sql;
-                    if ($sql) {
-                     
-                        echo "<script>alert('Inscription reussie')</script>";
-                        $sql->closeCursor();
-                    }
-            } catch (\Throwable $th) {
-                // echo $th->getMessage();
-                $sql->closeCursor();
-            }
+                    return $sql;
         }
        
         public function updateUser(){ 
